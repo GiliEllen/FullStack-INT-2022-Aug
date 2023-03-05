1945669abf4306f8844c16739e2720e36697ec1c
Gili <gilimena95@gmail.com>
2023-03-05 20:38:02 +0200

    WIP

--- Node.js/01-basics/example02/server.ts
+++ Node.js/01-basics/example02/server.ts
@@ -5,26 +5,48 @@ const app = express();
 const port = 4000;
 
 app.use(express.json());
-app.use(express.static("public")) // response with files
+app.use(express.static("public")); // response with files
 
-app.get("/", (req:express.Request, res:express.Response) => {
+app.get("/", (req: express.Request, res: express.Response) => {
   res.send("hello");
 });
 
 app.get("/users", (req, res) => {
-    console.log(users)
-    res.send({users})
-})
+  console.log(users);
+  res.send({ users });
+});
+
+app.get("/tours", (req, res) => {
+  res.send({ tours });
+});
+app.get("/tours/:id", (req, res) => {
+  const { id } = req.params;
+  const tourId = Number(id);
+  const tourDB = tours.filter((tour) => {
+    return tour.id === tourId;
+  });
+  res.send({ tourDB });
+});
+
+app.post("/tours", (req, res) => {
+  console.log(req.body);
+  const { tourName, id, price } = req.body;
+  tours.push({ name: tourName, id, price });
+  res.send({ tours });
+});
+
+
+
+//max group size
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
 
-
 const users = [
-    {firstName: "gili", age: "restricted"},
-    {firstName: "esti", age: "also restricted"}
-]
+  { firstName: "gili", age: "restricted" },
+  { firstName: "esti", age: "also restricted" },
+];
 
 //Http methods
 //get - get information from server
