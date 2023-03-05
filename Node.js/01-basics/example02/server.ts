a22fe88e362d451f347a706a190a98421c17ca63
Gili <gilimena95@gmail.com>
2023-03-05 19:20:25 +0200

    WIP

--- Node.js/01-basics/example02/server.ts
+++ Node.js/01-basics/example02/server.ts
@@ -1,15 +1,18 @@
 import express from "express";
+import { Tour, tours } from "./tours";
 
 const app = express();
 const port = 4000;
 
 app.use(express.json());
+app.use(express.static("public")) // response with files
 
 app.get("/", (req:express.Request, res:express.Response) => {
   res.send("hello");
 });
 
 app.get("/users", (req, res) => {
+    console.log(users)
     res.send({users})
 })
 
@@ -22,3 +25,10 @@ const users = [
     {firstName: "gili", age: "restricted"},
     {firstName: "esti", age: "also restricted"}
 ]
+
+//Http methods
+//get - get information from server
+//post - send information and create somthing new
+//patch - update somthing small
+//put - update somthing big
+//delete - delete somthing
