c8a2e2d485812547b95a948fd47b2d016630ef92
Gili <gilimena95@gmail.com>
2023-03-19 16:25:54 +0200

    Create task4.js

---
+++ Node.js/02-core-modules/task/task4.js
@@ -0,0 +1,5 @@
+// 1. followup - create a server that listens on port 4000 and returns and logs connection
+// create a server that writes hello world on / 
+// create a server that when user connects to
+//   /api/users, will return the users in people.js file
+//   or all the blog files
